28eb4b5c343274de265e9f1646149ffb075be8a0
matsuo <t_matsuo@pignus.co.jp>
2026-03-26 21:19:36 +0900

    DDD

---
+++ ddd/entity.py
@@ -0,0 +1,20 @@
+from abc import ABC
+
+class Entity(ABC):
+    def __init__(self, id: str):
+        self._id = id
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self._id == other._id
+
+    def __hash__(self) -> int:
+        return hash(self._id)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(id={self._id!r})"
